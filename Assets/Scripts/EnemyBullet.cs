1d50284f6126f8eb56a58d733ae9168df0709db1
tadakun <kokoroko.arive@gmail.com>
2016-10-21 22:25:58 +0900

    [追加]各種Particle Effect追加(Resourcesフォルダ内に入ってます.)、敵が一定時間ごとに追尾弾(ホーミング弾)を発射するように。また、弾が一定時間経過後消失するように。

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBullet : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public int lifeTime = 2;
+    int second = 0;
+    int counter = 0;
+
+    void Start()
+    {
+        second = 0;
+        counter = 0;
+    }
+
+    void Update()
+    {
+        second++;
+        float step = Time.deltaTime * speed;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        if(second > 60)
+        {
+            second = 0;
+            counter++;
+        }
+        if(counter > lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public int counter = 0;
+	// Use this for initialization
+	void Start () {
+        counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter++;
+	}
+}
