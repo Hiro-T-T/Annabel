5eeab161e14b38d058e2bdcaf304ec555fc7756a
tadakun <kokoroko.arive@gmail.com>
2016-10-24 18:53:51 +0900

    [追加]最低限

---
+++ Assets/Scripts/BlueDoorScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueDoorScript : MonoBehaviour {
+    public BlueImg bmg;
+	// Use this for initialization
+	void Start () {
+        bmg = GameObject.Find("BlueDoor").GetComponent<BlueImg>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(bmg.imgEn == true)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
