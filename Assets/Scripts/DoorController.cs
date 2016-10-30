4b0ae3069a73ef2fdf1322241daecf59fcfd0cac
tadakun <kokoroko.arive@gmail.com>
2016-10-30 16:49:28 +0900

    [追加]DoorController、Stage1(仮)

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+    public enum DoorType
+    {
+        Stage1_1,
+        Stage1_2,
+        Stage1_3
+    }
+
+    public DoorType doorType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
