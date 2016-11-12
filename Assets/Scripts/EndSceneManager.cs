aeaedd62ba0019f137e820d2e8ee81f7b7fa7d11
Mitsuhiko Hoshi <m011438553@edu.teu.ac.jp>
2016-11-13 08:24:43 +0900

    chin

--- Assets/Scripts/EndSceneManager.cs
+++ Assets/Scripts/EndSceneManager.cs
@@ -5,16 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class EndSceneManager : MonoBehaviour {
     TalkSettings talkSet;
+    FlagsInStageManager flagsInStageManager;
+    float count;
 	// Use this for initialization
 	void Start () {
         GameObject.FindGameObjectWithTag("EndImg").GetComponent<Image>().enabled = false;
         talkSet = GameObject.Find("GameControlObject").GetComponent<TalkSettings>();
+        flagsInStageManager = GameObject.Find("GameControlObject").GetComponent<FlagsInStageManager>();
         GameManager.clear = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if(talkSet.count == 1)
+        if(flagsInStageManager.talkMode != 0)
+        {
+            count += Time.deltaTime;
+        }
+      
+	    if(count > 1)
         {
             GameObject.FindGameObjectWithTag("EndImg").GetComponent<Image>().enabled = true;
             Invoke("MoveScene", 3.0F);
